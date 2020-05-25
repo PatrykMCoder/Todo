55dd39f67230daa707b958c21bcedb89e0fe2a37
PatrykMCoder <patrykmarciszewski3@gmail.com>
2020-05-25 21:30:15 +0200

    Update method for save todo and view Added card view like in edit todo

--- app/src/main/java/com/example/todo/view/fragments/AddNewTodoFragment.java
+++ app/src/main/java/com/example/todo/view/fragments/AddNewTodoFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -50,9 +49,14 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
     private String task;
     private String tag;
     private int done;
-    private int createdElement = 1;
+    private int createdElement = 0;
+    private View rootView;
 
     private CreateTodoHelper createTodoHelper;
+
+    private LinearLayout l;
+
+    // TODO: 25/05/2020 FIX IT BECAUSE IT IS VERY DIRTY
 
     public AddNewTodoFragment() {
         // Required empty public constructor
@@ -69,34 +73,40 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        final View rootView = inflater.inflate(R.layout.fragment_add_new_todo, container, false);
+        rootView = inflater.inflate(R.layout.fragment_add_new_todo, container, false);
 
         newTitleEditText = rootView.findViewById(R.id.new_title_todo);
         saveTodoButton = rootView.findViewById(R.id.save_todo);
         setTagButton = rootView.findViewById(R.id.set_tag);
         box = rootView.findViewById(R.id.box);
+        l = rootView.findViewById(R.id.box_new_item);
 
         data = new ArrayList<>();
 
         saveTodoButton.setOnClickListener(this);
         setTagButton.setOnClickListener(this);
+        l.setOnClickListener(this);
         createElements();
         return rootView;
     }
 
     @Override
     public void onClick(View view) {
-       switch (view.getId()) {
-           case R.id.save_todo:{
-               saveTodo();
-               break;
-           }
-           case R.id.set_tag: {
+        switch (view.getId()) {
+            case R.id.save_todo: {
+                saveTodo();
+                break;
+            }
+            case R.id.set_tag: {
                 DialogFragment dialogFragment = new SelectTodoTagFragment();
                 dialogFragment.show(getFragmentManager(), "set tag todo");
                 break;
-           }
-       }
+            }
+            case R.id.box_new_item: {
+                createElements();
+                break;
+            }
+        }
     }
 
     @Override
@@ -114,15 +124,17 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
         return super.onOptionsItemSelected(item);
     }
 
+
     private void createElements() {
         LinearLayout linearLayout = new LinearLayout(context);
         linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
 
+
         checkBoxDone = new CheckBox(context);
         newTaskEditText = new EditText(context);
         newTaskEditText.setId(createdElement);
-        newTaskEditText.setHint("Enter task(press enter to submit end create other edit text)");
+        newTaskEditText.setHint("Enter task");
         newTaskEditText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         newTaskEditText.setBackgroundColor(Color.WHITE);
         newTaskEditText.setTextSize(20);
@@ -130,20 +142,16 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
         newTaskEditText.setInputType(InputType.TYPE_CLASS_TEXT);
         newTaskEditText.setTextColor(Color.BLACK);
 
+        checkBoxDone.setTag("c_" + createdElement);
+        newTaskEditText.setTag("t_" + createdElement);
+
         newTaskEditText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN) {
                     if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                        if (!newTaskEditText.getText().toString().isEmpty()) {
-                            title = newTitleEditText.getText().toString();
-                            task = newTaskEditText.getText().toString();
-                            done = checkBoxDone.isChecked() ? 1 : 0;
-                            createTodoHelper = new CreateTodoHelper(task, done, tag, getCurrentDateString());
-                            data.add(createTodoHelper);
-                            createElements();
-                            return true;
-                        }
+                        createElements();
+                        return true;
                     }
                 }
                 return false;
@@ -155,6 +163,7 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
 
         box.addView(linearLayout);
         createdElement++;
+
     }
 
     private String getCurrentDateString() {
@@ -163,41 +172,34 @@ public class AddNewTodoFragment extends Fragment implements View.OnClickListener
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM hh:mm");
         date = simpleDateFormat.format(calendar.getTime());
-        Log.d(TAG, "getCurrentDateString: " + date);
         return date;
     }
 
     private void saveTodo() {
         title = newTitleEditText.getText().toString();
         if (!title.isEmpty()) {
-            task = newTaskEditText.getText().toString();
-            done = checkBoxDone.isChecked() ? 1 : 0;
-            tag = TagsHelper.getTag();
-            createTodoHelper = new CreateTodoHelper(task, done, tag, getCurrentDateString());
-            data.add(createTodoHelper);
+            data.clear();
 
+            for (int i = 0; i < createdElement; i++) {
+                newTaskEditText = rootView.findViewWithTag("t_" + i);
+                checkBoxDone = rootView.findViewWithTag("c_" + i);
 
-            for (int i = 0; i < data.size(); i++) {
-                data.get(i).setTag(TagsHelper.getTag());
+                task = newTaskEditText.getText().toString();
+                done = checkBoxDone.isChecked() ? 1 : 0;
+                tag = TagsHelper.getTag();
+
+                createTodoHelper = new CreateTodoHelper(task, done, tag, getCurrentDateString());
+                data.add(createTodoHelper);
+
             }
 
             todoAdapter = new TodoAdapter(context, title, data);
             todoAdapter.openDB();
-
-            if (!title.isEmpty()) {
-                if (data != null && data.size() > 0) {
-                    todoAdapter.saveToDB();
-                } else
-                    Toast.makeText(context, "You can't save empty TODO 😞", Toast.LENGTH_SHORT).show();
-            } else {
-                // TODO: 05/02/2020 show dialog with information about set title for todo
-            }
-
+            todoAdapter.saveToDB();
             todoAdapter.closeDB();
-            mainActivity.closeFragment(this, new TodoFragment());
 
-            TagsHelper.setTag("No tag");
-        }else
+            mainActivity.closeFragment(this, new TodoFragment());
+        } else
             Toast.makeText(context, "Title can not be empty.", Toast.LENGTH_SHORT).show();
     }
 }
