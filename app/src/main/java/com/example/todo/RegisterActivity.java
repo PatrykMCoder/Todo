7ce8b35cc8d269aa9214c792dd141c513ca313ad
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-07-17 23:10:45 +0200

    110 Create layout for login and register
    Has been added layout for login and register110 Create layout for login
    and register
    
    Service login register
    
    Created system for connect with API and login or register user
    Validation for fields in form login and register
    
    Update application todo and connect with API
    
    Added handle error and information for actions
    added load titles todo to view
    added load data to tododetails view
    added create todo
    added delete todo
    added dedit todo
    
    Add auto-login
    
    If user login once last have saved userID. On splash screen is checking
    value. If exist skip login activity
    
    added method for update todos afrer tap on checkbox
    
    fix bug [idk how... rly. but is tested and working
    
    128 Add checking internet connection
    
    Added method on start app for checking internet connection
    
    Create simple UI for profile fragment
    
    Load user data
    
    Create async class for load data
    Fill data in ui
    
    Add refreshing
    
    After slide from up to down are refreshing data(on todo list,
    todo details and profile)
    
    added save tags to db
    
    110 Create layout for login and register
    
    Has been added layout for login and register110 Create layout for login
    and register
    
    Service login register
    
    Created system for connect with API and login or register user
    Validation for fields in form login and register
    
    Update application todo and connect with API
    
    Added handle error and information for actions
    added load titles todo to view
    added load data to tododetails view
    added create todo
    added delete todo
    added dedit todo
    
    Add auto-login
    
    If user login once last have saved userID. On splash screen is checking
    value. If exist skip login activity
    
    added method for update todos afrer tap on checkbox
    
    fix bug [idk how... rly. but is tested and working
    
    128 Add checking internet connection
    
    Added method on start app for checking internet connection
    
    Create simple UI for profile fragment
    
    Load user data
    
    Create async class for load data
    Fill data in ui
    
    Add refreshing
    
    After slide from up to down are refreshing data(on todo list,
    todo details and profile)
    
    added save tags to db
    
    Added logout
    
    1. Add action for button logout. When user tap this:
    	- remove id from device
    	- start login activity
    
    Remove % done counter and replace it with tag view
    
    Added edit todo tag in edit todo fragment
    
    replaced toast with snack

---
+++ app/src/main/java/com/example/todo/RegisterActivity.java
@@ -0,0 +1,126 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.todo.service.MongoDBClient;
+import com.example.todo.service.MongodbHelper;
+import com.example.todo.service.Operation;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText, usernameEditText, confirmPasswordEditText;
+    private Button createUserButton;
+    private RelativeLayout rootView;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        rootView = findViewById(R.id.root_view);
+        usernameEditText = findViewById(R.id.username_edit_text);
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createUserButton = findViewById(R.id.button_login);
+        createUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create validation
+                String username = usernameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString();
+                String passwordConfirm = confirmPasswordEditText.getText().toString();
+
+                if (!username.isEmpty() && !email.isEmpty() && password.length() >= 8) {
+                    if (passwordConfirm.equals(password)) {
+                        progressDialog = ProgressDialog.show(RegisterActivity.this, "Create account", "Please wait...");
+                        RegisterThread registerThread = new RegisterThread(username, email, password);
+                        registerThread.execute();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Password are not this same or ", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Filed cannot be empty or password length should be 8 characters!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    class RegisterThread extends AsyncTask<String, String, String> {
+        private String username, password, email;
+
+        RegisterThread(String username, String email, String password) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            MongoDBClient mongoDBClient = new MongoDBClient(username, email, password);
+            int code = mongoDBClient.createUser();
+
+            if (code == 201)
+                return "Done";
+
+            return "NotDone";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Handler handler = new Handler();
+            if (s.equals("Done")) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(RegisterActivity.this, "User created!😊", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } else {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Snackbar.make(rootView, "Something wrong, try again", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                progressDialog.dismiss();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/todo/view/fragments/UserProfileFragment.java
@@ -0,0 +1,154 @@
+package com.example.todo.view.fragments;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.todo.LoginActivity;
+import com.example.todo.MainActivity;
+import com.example.todo.R;
+import com.example.todo.helpers.HideAppBarHelper;
+import com.example.todo.service.MongoDBClient;
+import com.example.todo.service.jsonhelper.JSONHelperUser;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class UserProfileFragment extends Fragment implements View.OnClickListener {
+    private View rootView;
+    private MainActivity mainActivity;
+    private ScrollView scrollView;
+    private RelativeLayout userWelcomeLayout;
+    private TextView welcomeUserTextView, usernameTextView, emailTextView;
+    private FrameLayout frameLayout;
+    private ImageView waveImage;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Button logoutButton;
+
+    private Context context;
+    private String userID;
+    private ArrayList<JSONHelperUser> userData;
+    private JSONHelperUser userObject;
+    private ProgressDialog progressDialog;
+    private SharedPreferences sharedPreferencesUserData;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) context;
+        this.context = context;
+        new HideAppBarHelper(mainActivity).hideBar();
+        sharedPreferencesUserData = context.getSharedPreferences("user_data", Context.MODE_PRIVATE);
+        userID = sharedPreferencesUserData.getString("user_id", null);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_user_profile, null);
+
+        welcomeUserTextView = rootView.findViewById(R.id.welcome_text);
+        usernameTextView = rootView.findViewById(R.id.user_name);
+        emailTextView = rootView.findViewById(R.id.user_email);
+        swipeRefreshLayout = rootView.findViewById(R.id.swipe_refresh);
+        logoutButton = rootView.findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(this);
+        progressDialog = ProgressDialog.show(context, "Loading data", "Please wait...");
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                LoadUserDataAsync loadUserDataAsync = new LoadUserDataAsync();
+                loadUserDataAsync.execute();
+            }
+        });
+
+        LoadUserDataAsync loadUserDataAsync = new LoadUserDataAsync();
+        loadUserDataAsync.execute();
+        return rootView;
+    }
+
+    private void updateUI(String username, String email){
+        if (username.length() > 15)
+            username = username.substring(0, Math.min(username.length(), 15)) + "...";
+        if(email.length() >= 15)
+            email = email.substring(0, Math.min(email.length(), 15)) + "...";
+
+        welcomeUserTextView.setText(String.format("Welcome\n%s", username));
+        usernameTextView.setText(String.format("Username: %s", username));
+        emailTextView.setText(String.format("Email: %s", email));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_logout: {
+                sharedPreferencesUserData.edit().putStringSet("user_id", null).apply();
+                Intent intent = new Intent(mainActivity, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
+
+    class LoadUserDataAsync extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            MongoDBClient mongoDBClient = new MongoDBClient();
+            String data = mongoDBClient.loadDataUser(userID);
+            if(data != null){
+                Gson gson = new Gson();
+                userObject = gson.fromJson(data, JSONHelperUser.class);
+                return userObject != null ? "done" : "notDone";
+            }
+            return "notDone";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            swipeRefreshLayout.setRefreshing(false);
+            if(progressDialog != null)
+                progressDialog.dismiss();
+            if (s.equals("done")) {
+                updateUI(userObject.username, userObject.email);
+            } else {
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Snackbar.make(rootView, "Something wrong, try again. Slide down to refresh", Snackbar.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+    }
+}
