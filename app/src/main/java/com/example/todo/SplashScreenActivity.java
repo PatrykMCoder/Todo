1f41160bef741125e6e9cf06b5f4e67d8639ffbb
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-08-08 01:35:51 +0200

    128 Add checking internet connection
    Added method on start app for checking internet connection

--- app/src/main/java/com/example/todo/SplashScreenActivity.java
+++ app/src/main/java/com/example/todo/SplashScreenActivity.java
@@ -1,9 +1,15 @@
 package com.example.todo;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.todo.utils.notifications.Notification;
@@ -17,7 +23,14 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         Notification notification = new Notification(getApplicationContext());
         notification.createNotificationsChanel();
+
         lunchActivity();
+    }
+
+    private boolean connected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 
     private String getUserID() {
@@ -28,13 +41,39 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     private void lunchActivity() {
         Intent intent;
+        if (connected()) {
+            if (getUserID() != null)
+                intent = new Intent(this, MainActivity.class);
+            else
+                intent = new Intent(this, LoginActivity.class);
 
-        if (getUserID() != null)
-            intent = new Intent(this, MainActivity.class);
-        else
-            intent = new Intent(this, LoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } else {
+            showAlertDialog();
+        }
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Needed internet connection")
+                .setMessage("Please, check your internet connection")
+                .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        lunchActivity();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(1);
+                    }
+                })
+                .setIcon(R.drawable.ic_baseline_warning_24);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
